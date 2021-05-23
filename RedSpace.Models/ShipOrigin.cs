9543280c42e690b82874d0eed2a17f5e12f308da
laamaj92@gmail.com <laamaj92@gmail.com>
2021-05-23 16:49:25 +0200

    shiporigin

---
+++ RedSpace.Models/ShipOrigin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedSpace.Models
+{
+    public class ShipOrigin
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Location { get; set; }
+        [Required]
+        public string ShipName { get; set; }
+        [Required]
+        public int CrewCapacity { get; set; }
+    }
+}
